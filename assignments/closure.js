8627ebaf49c6481c3e6eb10382ec9de7799b6256
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-04-02 17:50:16 -0400

    Updated responses for Callbacks file

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,7 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 function greeting(name) {
-	const englishGreet = "Hello"
+	const englishGreet = "Hello";
 	console.log(`${englishGreet} ${name}!`);
 
 	function spanish() {
@@ -22,16 +22,16 @@ greeting("Daisy");
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+// const counter = () => {
+//   // Return a function that when invoked increments and returns a counter variable.
 
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// // Example usage: const newCounter = counter();
+// // newCounter(); // 1
+// // newCounter(); // 2
 
-// ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+// // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// const counterFactory = () => {
+//   // Return an object that has two methods called `increment` and `decrement`.
+//   // `increment` should increment a counter variable in closure scope and return it.
+//   // `decrement` should decrement the counter variable and return it.
+// };
